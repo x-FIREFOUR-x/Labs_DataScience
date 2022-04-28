670d8a82c5c49e785fe7a5d74bebef8ac5bf3e74
Alex <olexandrpasalsky@gmail.com>
2022-04-28 21:24:53 +0300

    group fun in fun correction_error_in_date

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ pd.set_option('display.max_colwidth', None)
 desired_width = 320
 pd.set_option('display.width', desired_width)
 
+
     #зчитати дасет в датафрейм
 def read_dataset(path):
     data = pd.read_csv(path, sep=';', encoding='cp1252')
     return data
 
+
     #вивести датасет
 def print_dataset(dataset):
    print(dataset)
+
 
     #конвертувати тип даних колонки column_name датафрейма з string в float
 def convert_column_str_to_float(dataset, column_name):
@@ -22,19 +25,19 @@ def convert_column_str_to_float(dataset, column_name):
         .str.replace(',', '.')\
         .astype(float)
 
+
     #замінити NaN на середні значення
 def replace_nan_to_avarege(dataset):
     return dataset.fillna(dataset.mean())
+
 
     #замінити від'ємні значення в колонці column_name на їх абсолютні значення
 def abs_date(dataset, column_name):
     dataset[column_name] = dataset[column_name].abs()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    data_path = 'data\Data2.csv'
-    dataset = read_dataset(data_path)
 
+    #Виправити помилки в даних (типиб заміна пустих і відємних значень)
+def correction_error_in_date(dataset):
     convert_column_str_to_float(dataset, 'GDP per capita')
     convert_column_str_to_float(dataset, 'CO2 emission')
     convert_column_str_to_float(dataset, 'Area')
@@ -46,4 +49,15 @@ if __name__ == '__main__':
     abs_date(dataset, 'CO2 emission')
     abs_date(dataset, 'Area')
 
+    return dataset
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    data_path = 'data\Data2.csv'
+    dataset = read_dataset(data_path)
+
+    dataset = correction_error_in_date(dataset)
+
     print_dataset(dataset)
+    print(dataset.info())
