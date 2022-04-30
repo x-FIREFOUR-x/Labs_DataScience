c8d08a51aeee9cee666cdee8310c56df11dccdd3
Alex <olexandrpasalsky@gmail.com>
2022-04-30 15:47:14 +0300

    hist

--- main.py
+++ main.py
@@ -39,7 +39,23 @@ def print_neg_elements_dataset(dataset):
 def abs_date(dataset, column_name):
      dataset[column_name] = dataset[column_name].abs()
 
+def hist(dataset):
+    footer, hists = plt.subplots(1, 4, figsize=(14, 6))
+    footer.suptitle('Гістограми: ', fontsize=20)
 
+    hists[0].set_title('GDP per capita')
+    hists[0].hist(dataset['GDP per capita'])
+
+    hists[1].set_title('Population')
+    hists[1].hist(dataset['Populatiion'])
+
+    hists[2].set_title('CO2 emission')
+    hists[2].hist(dataset['CO2 emission'])
+
+    hists[3].set_title('Area')
+    hists[3].hist(dataset['Area'])
+
+    plt.show()
 
 if __name__ == '__main__':
     data_path = 'data\Data2.csv'
@@ -57,4 +73,9 @@ if __name__ == '__main__':
     abs_date(dataset, 'Area')
 
     #print(dataset.head(217))
-    #print(dataset.info())
+    print(dataset.info())
+
+    dataset.describe()
+
+    dataset.hist(figsize=(12,10))
+    plt.show()
