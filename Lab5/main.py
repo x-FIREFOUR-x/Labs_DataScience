01271bf7e9cd54db20b4de922a265f348a14aa7d
Alex <olexandrpasalsky@gmail.com>
2022-05-09 17:15:14 +0300

    libs

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,3 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+desired_width = 320
+pd.set_option('display.width', desired_width)
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
