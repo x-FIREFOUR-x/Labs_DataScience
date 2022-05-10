5bf2bcb18228f18cfc8338a98c6c5a63f1035617
Alex <olexandrpasalsky@gmail.com>
2022-05-10 12:07:02 +0300

    build model test model

--- Lab5/main.py
+++ Lab5/main.py
@@ -6,9 +6,11 @@ import scipy.stats as stats
 from sklearn.model_selection import train_test_split
 
 from sklearn.linear_model import LinearRegression
-
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -32,12 +34,12 @@ def is_Normal_ShpiroWilk(column, a = 0.05):
     return pvalue > a
 
 
-def delete_emissions(dataset, column_label):
+def delete_outliers(dataset, column_label):
     Q1 = dataset[column_label].quantile(0.25)
     Q3 = dataset[column_label].quantile(0.75)
-    IQR = Q3 - Q1
-    dataset = dataset.drop(dataset[(dataset[column_label] < (Q1 - 1.5 * IQR)) |
-                                   (dataset[column_label] > (Q3 + 1.5 * IQR))]
+    QR = Q3 - Q1
+    dataset = dataset.drop(dataset[(dataset[column_label] < (Q1 - 1.5 * QR)) |
+                                   (dataset[column_label] > (Q3 + 1.5 * QR))]
                            .index)
     return dataset
 
@@ -45,6 +47,7 @@ def delete_emissions(dataset, column_label):
 
 if __name__ == '__main__':
 
+    #main task
     data_path = 'data\winequality-red.csv'
     dataset = pd.read_csv(data_path, sep=',', encoding='cp1252')
 
@@ -61,10 +64,31 @@ if __name__ == '__main__':
         print(is_Normal_ShpiroWilk(dataset[column]))
 
     for column in dataset.columns:
-        dataset = delete_emissions(dataset, column)
+        dataset = delete_outliers(dataset, column)
 
     X = dataset.iloc[:, :11].to_numpy()
     Y = dataset['quality'].to_numpy()
 
-    X_train, X_test, Y_train, T_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.3, random_state=0)
 
+        #Побудуємо лінійну модель для прогнозу quility
+    lineRegresion = LinearRegression().fit(Xtrain, Ytrain)
+
+        #Побудуємо поліноміальну модель для прогнозу quility 2-4 порядків
+    Regresions = [lineRegresion]
+    for i in range(2, 4):
+        Regresions.append(make_pipeline(PolynomialFeatures(degree=i), LinearRegression()))
+        Regresions[i - 1].fit(Xtrain, Ytrain)
+
+        #Обрахуємо Середня квадратичну помилку(mse) коефіцієнт детермінації(r2)
+    mse = []
+    r2 = []
+    for r in Regresions:
+        pred = r.predict(Xtest)
+        mse.append(mean_squared_error(Ytest, pred))
+        r2.append(r2_score(Ytest, pred))
+
+    degree = list(range(1, 4))
+    plt.plot(degree, mse, color='red')
+    plt.plot(degree, r2, color='blue')
+    plt.show()
