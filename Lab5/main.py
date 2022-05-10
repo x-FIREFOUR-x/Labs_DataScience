f82810f6c2d2fce36afa52cd3bc8091124924d0d
Alex <olexandrpasalsky@gmail.com>
2022-05-10 09:24:41 +0300

    research and data shredding

--- Lab5/main.py
+++ Lab5/main.py
@@ -5,16 +5,42 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
-desired_width = 320
+desired_width = 250
 pd.set_option('display.width', desired_width)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_neg_elements_dataset(dataset):
+    for col_name in dataset.columns:
+        print(f'{col_name} neg values:\n', dataset[dataset[col_name] < 0])
 
 
-# Press the green button in the gutter to run the script.
+def print_nan_elements_dataset(dataset):
+    for col_name in dataset.columns:
+        print(f'{col_name} nan values:\n', dataset[dataset[col_name] < 0])
+
+
+def delete_emissions(dataset, column_label):
+    Q1 = dataset[column_label].quantile(0.25)
+    Q3 = dataset[column_label].quantile(0.75)
+    IQR = Q3 - Q1
+    dataset = dataset.drop(dataset[(dataset[column_label] < (Q1 - 1.5 * IQR)) |
+                                   (dataset[column_label] > (Q3 + 1.5 * IQR))]
+                           .index)
+    return dataset
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
+    data_path = 'data\winequality-red.csv'
+    dataset = pd.read_csv(data_path, sep=',', encoding='cp1252')
 
+    print(dataset.info())
+    print(dataset.head(10))
+
+    print_neg_elements_dataset(dataset)
+    print_nan_elements_dataset(dataset)
+
+    datasetcorr = dataset.corr()
+    print(datasetcorr)
+
+    for column in dataset:
+        dataset = delete_emissions(dataset, column)
