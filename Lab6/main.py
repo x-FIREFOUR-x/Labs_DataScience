268cbb14511083543533df1fa8e38a1376931204
Alex <olexandrpasalsky@gmail.com>
2022-05-15 16:28:15 +0300

    сlassifier

--- Lab6/main.py
+++ Lab6/main.py
@@ -2,8 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patch
-from sklearn.model_selection import train_test_split
 import seaborn as sbn
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.model_selection import cross_val_score
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -58,7 +65,7 @@ if __name__ == '__main__':
     print(missing_data.head(10))
     dataset = dataset.drop(columns=['Cabin'])
 
-    dataset_train, dataset_test = train_test_split(dataset, test_size=0.3, random_state=1)
+    dataset_train, dataset_test = train_test_split(dataset, test_size=0.2, random_state=1)
 
     dataset_train = dataset_train.fillna(dataset_train.mean())
     dataset_test = dataset_test.fillna(dataset_test.mean())
@@ -77,5 +84,44 @@ if __name__ == '__main__':
 
     features = pd.concat([dataset_train, dataset_test]).reset_index(drop=True)
     features = pd.get_dummies(features)
-    df_train = features.iloc[:dataset_train.shape[0], :]
-    df_test = features.iloc[dataset_train.shape[0]:, :]
+    dataset_train = features.iloc[:dataset_train.shape[0], :]
+    dataset_test = features.iloc[dataset_train.shape[0]:, :]
+
+
+    Xtrain = dataset_train.drop(columns='Survived')
+    Ytrain = dataset_train['Survived']
+
+    Xtest = dataset_test.drop(columns='Survived')
+    Ytest = dataset_test['Survived']
+
+    decision_tree = DecisionTreeClassifier(max_depth=3, random_state=1)
+    tree_scores = cross_val_score(decision_tree, Xtrain, Ytrain, cv=5)
+    print('Decision Tree cvs:', tree_scores.mean())
+    decision_tree.fit(Xtrain, Ytrain)
+    print('Decision Tree test:', decision_tree.score(Xtest, Ytest))
+
+    random_forest = RandomForestClassifier(max_depth=5)
+    forest_scores = cross_val_score(random_forest, Xtrain, Ytrain, cv=5)
+    print('Random forest cvs:', forest_scores.mean())
+    random_forest.fit(Xtrain, Ytrain)
+    print('Random forest test:', random_forest.score(Xtest, Ytest))
+
+    boost = AdaBoostClassifier(learning_rate=0.3)
+    boost_scores = cross_val_score(boost, Xtrain, Ytrain, cv=5)
+    print('AdaBoost cvs:', boost_scores.mean())
+    boost.fit(Xtrain, Ytrain)
+    AdaBoostClassifier(learning_rate=0.3)
+    print('AdaBoost test:', boost.score(Xtest, Ytest))
+
+    gradboost = GradientBoostingClassifier(learning_rate=0.1)
+    gradboost_scores = cross_val_score(gradboost, Xtrain, Ytrain, cv=5)
+    print('GradientBoosting cvs:', gradboost_scores.mean())
+    gradboost.fit(Xtrain, Ytrain)
+    GradientBoostingClassifier(learning_rate=0.1)
+    print('GradientBoosting test:', gradboost.score(Xtest, Ytest))
+
+    bagging = GradientBoostingClassifier(learning_rate=0.1)
+    bagging_scores = cross_val_score(gradboost, Xtrain, Ytrain, cv=5)
+    print('Bagging cvs:', bagging_scores.mean())
+    bagging.fit(Xtrain, Ytrain)
+    print('Bagging test:', bagging.score(Xtest, Ytest))
