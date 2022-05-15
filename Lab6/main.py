3e1b140bf86df565ea2b1cf772bb0eb6fec8b6e8
Alex <olexandrpasalsky@gmail.com>
2022-05-15 12:53:06 +0300

    read, struct

--- Lab6/main.py
+++ Lab6/main.py
@@ -1,8 +1,28 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+desired_width = 300
+pd.set_option('display.width', desired_width)
+
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    data_path = 'data\\titanic.csv'
+    dataset = pd.read_csv(data_path, sep=',', encoding='cp1252', decimal='.')
 
+    print(dataset.info())
+    print(dataset.head(10))
+
+    dataset['Pclass'] = dataset['Pclass'].astype(str)
+    print(dataset.info())
+
+    dataset = dataset.drop(columns=['PassengerId'])
+    dataset = dataset.drop(columns=['Name'])
+
+    print(dataset.info())
+    
