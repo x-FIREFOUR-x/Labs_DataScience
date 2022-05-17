357200bfff4b70110a84d3b664c870325ee91de6
Alex <olexandrpasalsky@gmail.com>
2022-05-17 12:13:36 +0300

    graphics

--- Lab7/main.py
+++ Lab7/main.py
@@ -12,7 +12,7 @@ pd.set_option('display.width', desired_width)
 
 
 if __name__ == '__main__':
-    
+
     #Основне завдання 1
     data_path = 'data\\Covid.csv'
     dataset = pd.read_csv(data_path, sep=',', encoding='cp1252', decimal='.')
@@ -29,11 +29,22 @@ if __name__ == '__main__':
     print(dataset.info())
     print(dataset.head(2))
 
-    countries = (dataset.groupby(['Entity']))['Entity']
-    print(countries.head(1))
+    print((dataset.groupby(['Entity']))['Entity'].head(1))
     dataset = dataset[(dataset['Entity'] == 'Ukraine') | (dataset['Entity'] == 'Poland')]
+    print((dataset.groupby(['Entity']))['Entity'].head(1))
 
-    countries = (dataset.groupby(['Entity']))['Entity']
-    print(countries.head(1))
+    print('negative values:\n', dataset[dataset['Cases'] < 0])
 
-    print('GDP per capita negative values:\n', dataset[dataset['Cases'] < 0])
+    print(dataset.head(5))
+
+    dataset = pd.pivot_table(dataset, values=['Cases'], index=['Date'], columns=['Entity'])
+    print(dataset.info())
+    print(dataset['Cases']['Ukraine'].head(10))
+
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    dataset['Cases'][['Ukraine', 'Poland']].plot(ax=ax)
+    plt.title('Часова динаміка Covid-19 в Україні та Польщі')
+    ax.grid()
+    plt.show()
+
