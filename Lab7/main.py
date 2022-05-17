854333f8938d41c6fa3e3968a14d5cd9f84c560f
Alex <olexandrpasalsky@gmail.com>
2022-05-17 13:57:53 +0300

    fin main task 1

--- Lab7/main.py
+++ Lab7/main.py
@@ -2,11 +2,39 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+import statsmodels.api as sm
+import statsmodels.tsa.api as smt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_colwidth', None)
 desired_width = 300
 pd.set_option('display.width', desired_width)
+
+
+    #згладжування за допомогою ковзаючого середнього
+def plot_moving_average(series, n):
+    rolling_mean = series.rolling(window=n).mean()
+    plt.figure(figsize=(15, 5))
+    plt.title(f'Moving average\n window size = {n}')
+    plt.plot(rolling_mean, c='orange', label='Rolling mean trend')
+    plt.plot(series[n:], label='Actual values')
+    plt.legend(loc='upper left')
+    #plt.grid(True)
+    plt.show()
+
+
+    #Перевірка ряду на стаціонарність за допомогою доповненого тесту Дікі-Фуллера:
+def dickey_fuller_test(series):
+    test = smt.adfuller(series, autolag='AIC')
+    print('adf: ', test[0])
+    print('p-value: ', test[1])
+    print('Critical values: ', test[4])
+    if test[0] > test[4]['5%']:
+        print('Наявні одиничні корені, ряд не стаціонарний.')
+    else:
+        print('Одиничні корені відсутні, ряд є стаціонарним.')
 
 
 
@@ -49,11 +77,11 @@ if __name__ == '__main__':
     plt.show()
 
 
-    pl_covid_ds = covid_ds['Cases'][['Poland']]
-    pl_covid_ds.describe()
-
     ukr_covid_ds = covid_ds['Cases'][['Ukraine']]
     ukr_covid_ds.describe()
+
+    pl_covid_ds = covid_ds['Cases'][['Poland']]
+    pl_covid_ds.describe()
 
     fig, ax = plt.subplots(1, 2, figsize=(20, 8))
     ax[0].set_title('Ukraine')
@@ -65,4 +93,36 @@ if __name__ == '__main__':
     ax[1].hist(pl_covid_ds)
 
     plt.show()
+
+
+    plot_moving_average(ukr_covid_ds, 5)
+    plot_moving_average(ukr_covid_ds, 10)
+    plot_moving_average(ukr_covid_ds, 20)
+
+    plot_moving_average(pl_covid_ds, 5)
+    plot_moving_average(pl_covid_ds, 10)
+    plot_moving_average(pl_covid_ds, 20)
+
+    '''
+    decomposition = smt.seasonal_decompose(ukr_covid_ds[~ukr_covid_ds.isna()])
+    fig = decomposition.plot()
+    fig.set_size_inches(15, 10)
+    plt.show()
+    '''
+
+    fig, ax = plt.subplots(2, figsize=(15, 10))
+    fig.suptitle('Ukraine: ', fontsize=20)
+    ax[0] = plot_acf(ukr_covid_ds[~ukr_covid_ds.isna()], ax=ax[0], lags=120)
+    ax[1] = plot_pacf(ukr_covid_ds[~ukr_covid_ds.isna()], ax=ax[1], lags=120)
+    plt.show()
+
+    fig, ax = plt.subplots(2, figsize=(15, 10))
+    fig.suptitle('Poland', fontsize=20)
+    ax[0] = plot_acf(pl_covid_ds[~pl_covid_ds.isna()], ax=ax[0], lags=120)
+    ax[1] = plot_pacf(pl_covid_ds[~pl_covid_ds.isna()], ax=ax[1], lags=120)
+    plt.show()
+
+    dickey_fuller_test(ukr_covid_ds[~ukr_covid_ds.isna()])
+    #dickey_fuller_test(pl_covid_ds[~pl_covid_ds.isna()])
+
 
