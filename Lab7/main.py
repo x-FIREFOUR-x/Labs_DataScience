b68596997dd4b919ae834f4ba020af4031022304
Alex <olexandrpasalsky@gmail.com>
2022-05-17 10:52:35 +0300

    main task1: download, read, fix struct eror data

--- Lab7/main.py
+++ Lab7/main.py
@@ -1,9 +1,39 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
+desired_width = 300
+pd.set_option('display.width', desired_width)
+
 
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    
+    #Основне завдання 1
+    data_path = 'data\\Covid.csv'
+    dataset = pd.read_csv(data_path, sep=',', encoding='cp1252', decimal='.')
 
+    print(dataset.info())
+    print(dataset.head(2))
 
+    dataset = dataset.drop(columns=
+                           ['Continent', 'Latitude', 'Longitude', 'Average temperature per year',
+                            'Hospital beds per 1000 people', 'Medical doctors per 1000 people',
+                            'GDP/Capita', 'Population', 'Median age',
+                            'Population aged 65 and over (%)', 'Daily tests', 'Deaths'
+                            ])
+    print(dataset.info())
+    print(dataset.head(2))
+
+    countries = (dataset.groupby(['Entity']))['Entity']
+    print(countries.head(1))
+    dataset = dataset[(dataset['Entity'] == 'Ukraine') | (dataset['Entity'] == 'Poland')]
+
+    countries = (dataset.groupby(['Entity']))['Entity']
+    print(countries.head(1))
+
+    print('GDP per capita negative values:\n', dataset[dataset['Cases'] < 0])
