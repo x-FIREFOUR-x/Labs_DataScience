2488e42f642c7b391402aa20b1cc9779804eb52d
Alex <olexandrpasalsky@gmail.com>
2022-05-23 21:10:54 +0300

    clean and tokenize

--- Lab8/main.py
+++ Lab8/main.py
@@ -1,6 +1,12 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+from nltk.tokenize import RegexpTokenizer
+import re
+import csv
+import io
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -8,7 +14,50 @@ pd.set_option('display.max_columns', None)
 desired_width = 1500
 pd.set_option('display.width', desired_width)
 
+def clean_data(dataset):
+    for index in range(dataset.index[0], dataset.index[-1]):
+        dataset.loc[index]['Title'] = re.sub(r"<[^>]+>", "", dataset.loc[index]['Title'])
+        dataset.loc[index]['Body'] = re.sub(r"<[^>]+>", "", dataset.loc[index]['Body'])
 
+        dataset.loc[index]['Title'] = re.sub('[^''A-Za-z0-9А-Яа-яҐґЄєІіЇї]+', " ", dataset.loc[index]['Title'])
+        dataset.loc[index]['Body'] = re.sub('[^''A-Za-z0-9А-Яа-яҐґЄєІіЇї]+', " ", dataset.loc[index]['Body'])
+
+        dataset.loc[index]['Title'] = dataset.loc[index]['Title'].lower()
+        dataset.loc[index]['Body'] = dataset.loc[index]['Body'].lower()
+
+    return dataset
+
+def get_stop_words(language):
+    stopwords = []
+    filename = 'data/stop_words_ru.csv'
+    if language == 'ukrainian':
+        filename = 'data/stop_words_ua.csv'
+    with io.open(filename, 'r', encoding="utf-8") as file:
+        for row in csv.reader(file):
+            stopwords.append(row[0])
+    return stopwords
+
+def get_abbreviation(language):
+    if language == 'ukrainian':
+        return ['тис', 'грн', 'т.я', 'вул', 'cек', 'хв', 'обл', 'кв', 'пл', 'напр', 'гл', 'і.о', 'зам']
+    return ['тыс', 'руб', 'т.е', 'ул', 'д', 'сек', 'мин', 'т.к', 'т.н', 'т.о', 'ср', 'обл', 'кв', 'пл',
+            'напр', 'гл', 'и.о', 'им', 'зам', 'гл', 'т.ч']
+
+
+def tokenize_del_stop_worlds(dataset):
+    nltk.download('punkt')
+    stop_words = get_stop_words('ukrainian')
+
+    for index in range(dataset.index[0], dataset.index[-1]):
+        tokens = nltk.word_tokenize(dataset.loc[index]['Title'])
+        text = [word for word in tokens if word not in stop_words]
+        dataset['Title'].loc[index] = text
+
+        tokens = nltk.word_tokenize(dataset.loc[index]['Body'])
+        text = [word for word in tokens if word not in stop_words]
+        dataset['Body'].loc[index] = text
+
+    return dataset
 
 if __name__ == '__main__':
     data_path = 'data/ukr_text.csv'
@@ -16,3 +65,10 @@ if __name__ == '__main__':
 
     print(dataset.info())
     print(dataset.head())
+
+
+        #1. очистка 2. токенізація
+    data = clean_data(dataset)
+    print(data.head())
+    data = tokenize_del_stop_worlds(data)
+    print(data.head())
